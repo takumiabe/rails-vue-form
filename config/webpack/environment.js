af4d5155247889e4a1352ab9bb2fb65882259747
Takumi Abe <abe@engraphia.com>
2020-08-28 15:34:46 +0900

    Fix: templateをRailsで生成するときは必要になるようなので、@vueでVueのフルセットを参照できるようにする

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,11 @@ const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
+
+Object.assign(environment.config.resolve, {
+  alias: {
+    '@vue': 'vue/dist/vue.esm.js'
+  }
+})
+
 module.exports = environment
