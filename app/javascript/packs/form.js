553e323aca514465b0938cbd28bd89a8cbdb02f1
Takumi Abe <abe@engraphia.com>
2020-09-02 14:29:37 +0900

    Fix: 削除によってsort_keyがずれるので、変更にかかる部分をmethods化して、updateSortKeyを呼ぶように変更

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -7,6 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
     components: { draggable },
     data() {
       return { article: { comments: [] } }
+    },
+    methods: {
+      addComment() {
+        this.article.comments.push(
+          { name: "", body: "", sort_key: 1, _destroy: null }
+        );
+        this.updateSortKey();
+      },
+      delComment(comment) {
+        if(comment.id) {
+          this.$set(comment, '_destroy', 1);
+        } else {
+          let i = this.article.comments.findIndex(c => c === comment);
+          if (i >= 0) {
+            this.article.comments.splice(i, 1)
+          }
+        }
+        this.updateSortKey();
+      },
+      updateSortKey() {
+        this.article.comments.filter(comment => comment._destroy !== 1).forEach((comment, index) => {
+          comment.sort_key = index + 1;
+        });
+      },
     }
   });
   const data = JSON.parse(document.getElementById('form').dataset.init);
