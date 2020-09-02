b2209c5de99a075427eb54d2983f63c0d8f89988
Takumi Abe <abe@engraphia.com>
2020-09-02 13:57:07 +0900

    allow_destroy: true対応

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
-  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :comments, allow_destroy: true
 
   validates :title, presence: true
 end
